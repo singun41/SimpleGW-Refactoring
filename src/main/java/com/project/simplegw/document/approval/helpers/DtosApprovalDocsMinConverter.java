0c85a04fb7f28500298c4b8789da6364f61d8378
hcshin <singun41@gmail.com>
2022-07-06 17:14:38 +0900

    코드 수정

--- src/main/java/com/project/simplegw/document/approval/helpers/DtosApprovalDocsMinConverter.java
+++ src/main/java/com/project/simplegw/document/approval/helpers/DtosApprovalDocsMinConverter.java
@@ -5,11 +5,10 @@ import java.math.BigInteger;
 import com.project.simplegw.document.approval.dtos.send.DtosApprovalDocsMin;
 import com.project.simplegw.document.approval.vos.Sign;
 import com.project.simplegw.document.vos.DocsType;
-import com.project.simplegw.member.data.MemberData;
 
 public class DtosApprovalDocsMinConverter {
     // 받은 결재문서의 기간 검색 후 바인딩(ApproverService, ReferrerService에서 호출)
-    public static DtosApprovalDocsMin fromObjs(Object[] objs, MemberData approverInfo) {
+    public static DtosApprovalDocsMin fromObjs(Object[] objs) {
         BigInteger bigId = (BigInteger) objs[0];
 
         return new DtosApprovalDocsMin()
@@ -22,12 +21,12 @@ public class DtosApprovalDocsMinConverter {
             .setWriterJobTitle((String) objs[4])
             .setWriterName((String) objs[5])
             
-            .setApproverTeam(approverInfo.getTeam())
-            .setApproverJobTitle(approverInfo.getJobTitle())
-            .setApproverName(approverInfo.getName())
+            .setApproverTeam((String) objs[6])
+            .setApproverJobTitle((String) objs[7])
+            .setApproverName((String) objs[8])
             
-            .setSign(Sign.valueOf((String) objs[7]))
+            .setSign(Sign.valueOf((String) objs[9]))
 
-            .setCreatedDate( ((java.sql.Date) objs[8]).toLocalDate() );
+            .setCreatedDate( ((java.sql.Date) objs[10]).toLocalDate() );
     }
 }
