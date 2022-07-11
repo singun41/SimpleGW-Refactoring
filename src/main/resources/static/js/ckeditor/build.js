628016dbb0f358ada83e51038a43bc69325b0761
hcshin <singun41@gmail.com>
2022-07-11 16:13:56 +0900

    ckeditor table custom class & css 설정

--- src/main/resources/static/js/ckeditor/build.js
+++ src/main/resources/static/js/ckeditor/build.js
@@ -15,6 +15,10 @@ function buildCkEditor() {
             info.get( 'txtBorder' )[ 'default' ] = '1';         // Set default border to 1
             info.get( 'txtCellPad' )[ 'default' ] = '1';
             info.get( 'txtCellSpace' )[ 'default' ] = '0';
+
+            // 다른 css에 의해서 테이블 렌더링이 핸들링 되지 않도록 커스텀 클래스를 기본으로 지정한다. common.css에 해당 클래스에 대한 렌더링을 추가로 설정함.
+            let customClass = dialogDefinition.getContents('advanced').get('advCSSClasses');
+            customClass['default'] = 'ckeditor-tables';
         }
     });
     
