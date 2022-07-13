0261ea5bd7ca5ecf46557412bbec1d68da76e3d0
hcshin <singun41@gmail.com>
2022-07-13 14:02:07 +0900

    기본결재문서 endpoint mapping을 위해 코드 추가

--- src/main/resources/static/js/docs/approval/default/modify.js
+++ src/main/resources/static/js/docs/approval/default/modify.js
@@ -1,3 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    setApprovalLines();
+});
+
+const docsId = document.getElementById('docsId').innerText;
+const docsType = document.getElementById('docsType').innerText;
+
 async function update() {
     let params = {
         content: CKEDITOR.instances.ckeditorTextarea.getData()
@@ -6,5 +13,72 @@ async function update() {
     if(docsId) {
         saveComplete = true;
         location.href = '/page/approval/forms/' + docsType.toLowerCase() + '/' + docsId;
+    }
+}
+
+async function setApprovalLines() {
+    let response = await fetchGet('approval/line/' + docsId + '/' + docsType);
+    let result = await response.json();
+
+    if(response.ok) {
+        let approvers = result.obj.approvers;
+        let referrers = result.obj.referrers;
+
+        let approverIds = [];
+        let approverNames = [];
+        Array.from(approvers).forEach(e => { approverIds.push(e.memberId); });
+        Array.from(approvers).forEach(e => { approverNames.push(e.jobTitle + ' ' + e.name); });
+
+        let referrerIds = [];
+        let referrerNames = [];
+        Array.from(referrers).forEach(e => { referrerIds.push(e.memberId); });
+        Array.from(referrers).forEach(e => { referrerNames.push(e.jobTitle + ' ' + e.name); });
+
+        let approverLineData = {
+            approverIds: approverIds,
+            approverNames: approverNames,
+            referrerIds: referrerIds,
+            referrerNames: referrerNames
+        };
+        localStorage.setItem('approverLineData', JSON.stringify(approverLineData));
+
+        setLine();   // docs/approval/common/approvers-write.js 의 함수 setLine() 호출.
+    }
+}
+
+async function updateApprovalDocs(params) {
+    if(!confirm('수정하시겠습니까?'))
+        return 0;
+
+    // 모든 결재문서 공통 파라미터: 제목, 결재자, 참조자
+    params.title = document.getElementById('title').value;
+
+    // approvers.js에서 등록한 결재, 참조자 정보
+    params.arrApproverId = approverIds;
+    params.arrReferrerId = referrerIds;
+
+    let response = await fetchPatchParams('approval/forms/' + docsType.toLowerCase() + '/' + docsId, params);
+    let result = await response.json();
+    
+    if(response.ok) {
+        // 문서저장 후 첨부파일 저장 진행.
+        if(arrFile.length === 0) {
+            alert(result.msg);
+            return docsId;
+        }
+
+        let uploadResponse = await uploadFiles(docsId);
+        let uploadResult = await uploadResponse.json();
+        alert(uploadResult.msg);   // 파일 첨부 결과 메시지 띄우기.
+
+        if(uploadResponse.ok) {   // 첨부파일 완료 후 문서번호 리턴.
+            return docsId;
+        } else {
+            return 0;
+        }
+
+    } else {   // 문서 저장 실패시 메시지 띄우기.
+        alert(result.msg);
+        return 0;
     }
 }
--- src/main/resources/static/js/docs/approval/default/temp/modify.js
+++ src/main/resources/static/js/docs/approval/default/temp/modify.js
@@ -1,3 +1,6 @@
+const docsId = document.getElementById('docsId').innerText;
+const docsType = document.getElementById('docsType').innerText;
+
 async function save() {
     let params = {
         content: CKEDITOR.instances.ckeditorTextarea.getData()
@@ -6,7 +9,7 @@ async function save() {
 
     if(docsId) {
         saveComplete = true;
-        location.href = '/page/approval/' + docsType.toLowerCase() + '/' + docsId;
+        location.href = '/page/approval/forms/' + docsType.toLowerCase() + '/' + docsId;
     }
 }
 
@@ -18,6 +21,72 @@ async function updateTemp() {
     let docsId = await updateTempApprovalDocs(params);
     if(docsId) {
         saveComplete = true;
-        location.href = '/page/approval/' + docsType.toLowerCase() + '/temp/' + docsId;
+        location.href = '/page/approval/forms/' + docsType.toLowerCase() + '/temp/' + docsId;
     }
+}
+
+async function saveApprovalDocs(params) {
+    if(!confirm('등록하시겠습니까?'))
+        return 0;
+    
+    // 모든 결재문서 공통 파라미터: 제목, 결재자, 참조자
+    params.title = document.getElementById('title').value;
+
+    // approvers.js에서 등록한 결재, 참조자 정보
+    params.arrApproverId = approverIds;
+    params.arrReferrerId = referrerIds;
+
+    let response = await fetchPostParams('approval/forms/' + docsType.toLowerCase(), params);
+    let result = await response.json();
+
+    if(response.ok) {
+        // 문서저장 후 첨부파일 저장 진행.
+        if(arrFile.length === 0) {
+            alert(result.msg);
+            deleteTemp();   // 정상 등록하면 임시저장 문서를 삭제할 지 묻는다.
+            return result.obj;
+        }
+
+        let uploadResponse = await uploadFiles(result.obj);   // 리턴받은 obj 속성에 문서번호가 담겨있음.
+        let uploadResult = await uploadResponse.json();
+        alert(uploadResult.msg);   // 파일 첨부 결과 메시지 띄우기.
+
+        if(uploadResponse.ok) {   // 첨부파일 완료 후 문서번호 리턴.
+            deleteTemp();   // 정상 등록하면 임시저장 문서를 삭제할 지 묻는다.
+            return result.obj;
+
+        } else {
+            return 0;
+        }
+
+    } else {   // 문서 저장 실패시 메시지 띄우기.
+        alert(result.msg);
+        return 0;
+    }
+}
+
+async function updateTempApprovalDocs(params) {
+    if(!confirm('임시저장 하시겠습니까?' + '\n' + '첨부파일과 결재라인은 저장되지 않습니다.'))
+        return 0;
+    
+    // 모든 결재문서 임시저장 공통 파라미터: 제목
+    params.title = document.getElementById('title').value;
+
+    let response = await fetchPatchParams('approval/forms/' + docsType.toLowerCase() + '/temp/' + docsId, params);
+    let result = await response.json();
+    alert(result.msg);
+
+    if(response.ok)
+        return docsId;
+    else
+        return 0;
+}
+
+async function deleteTemp() {
+    if(!confirm('현재 임시저장 문서를 삭제하시겠습니까?'))
+        return;
+
+    let response = await fetchDelete('approval/forms/' +  docsType.toLowerCase() + '/temp/' + document.getElementById('docsId').innerText);
+    let result = await response.json();
+    alert(result.msg);
 }
