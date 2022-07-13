4ad703b3b0202c6e92b821994876b7ddcd310d7d
hcshin <singun41@gmail.com>
2022-07-13 16:37:54 +0900

    필드 추가

--- src/main/java/com/project/simplegw/document/approval/dtos/receive/details/dayoff/DtorDayoff.java
+++ src/main/java/com/project/simplegw/document/approval/dtos/receive/details/dayoff/DtorDayoff.java
@@ -2,6 +2,7 @@ package com.project.simplegw.document.approval.dtos.receive.details.dayoff;
 
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -17,6 +18,9 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @ToString(callSuper = true)
 public class DtorDayoff extends DtorDetailsCommon {
+    @NotBlank(message = "내용을 입력하세요.")
+    private String content;
+
     @Size(min = 1, message = "휴가 데이터를 추가하세요.")
     @NotNull(message = "휴가 데이터를 추가하세요.")
     private List<DtorDayoffDetails> detailList;
